a5f6d062f6929649014427cd40195739c48bac85
Kim <xzsa18@naver.com>
2022-10-10 16:55:24 +0900

    토스트 띄워보기

--- app/src/main/java/com/example/kotlinbasic01/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasic01/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.kotlinbasic01
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +19,9 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        SmallBtn.setOnClickListener {
 
+            Toast.makeText(this, "작은 버튼 눌림", Toast.LENGTH_SHORT).show()
+        }
     }
 }
